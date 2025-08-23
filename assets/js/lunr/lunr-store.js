fe5e9639441c415661f8ef6b7512f58ac5d50b8c
jude-shin <jude-shin@users.noreply.github.com>
2025-08-23 07:16:23 +0000

    jekyll build from Action 1713d42cb0e6cc036ec83822d720f4022f8907d4

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,16 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2025/08/10/welcome-to-jekyll.html",
+        "teaser": "/assets/images/teasers/clouds.jpg"
+      },{
+    "title": "Welcome to Jekyll!",
+    "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","url": "https://jude-shin.github.io/_posts/2025-08-10-welcome-to-jekyll.html"
+  },{
+    "title": "About",
+    "excerpt":"Compuer Science I am currently pursuing my Bachelors in Computer Science at California Polytechnic State University. During the school year I work part time as a software engineer, and during the summer I work full time. Rock Climbing Through all of this, I also try and make time to continue...","url": "https://jude-shin.github.io/about/"
+  },{
+    "title": "Welcome!",
+    "excerpt":"                                                                                                                     Rock Climbing                                                    See my journey as an outdoor rock climber, competitive climber, and youth competition coach.                                                   Learn more                                                                                                                                                    Computer Science                                                    Take a look at some of the work I have done in this field.                                                   Learn more                                                                                                                                                    Photography                                                    Just a gallery of my little side hobby.                                                   Learn more                                        ","url": "https://jude-shin.github.io/"
+  }]
