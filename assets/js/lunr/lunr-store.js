a713dec1c2e9b44a5526582ca0a9f9d421f4dd0a
jude-shin <jude-shin@users.noreply.github.com>
2025-09-14 07:48:50 +0000

    jekyll build from Action ff1d682d6c67c6651d243b7fe1b85da7618bebbd

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,40 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["vim"],
+        "tags": [],
+        "url": "/vim/2025/08/10/welcome-to-jekyll.html",
+        "teaser": null
+      },{
+        "title": "Balancing Coaching With Personal Training",
+        "excerpt":"this is just another test for collections  ","categories": ["climbing","coaching"],
+        "tags": [],
+        "url": "/climbing/coaching/2025/08/10/balancing-coaching-with-personal-training.html",
+        "teaser": null
+      },{
+        "title": "Youth Nationals",
+        "excerpt":"this is just a test to see how collections work. For now, there is nothing to see :)  ","categories": ["climbing"],
+        "tags": [],
+        "url": "/climbing/2025/08/10/youth-nationals.html",
+        "teaser": null
+      },{
+    "title": "Welcome to Jekyll!",
+    "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","url": "https://jude-shin.github.io/_posts/2025-08-10-welcome-to-jekyll.html"
+  },{
+    "title": "Balancing Coaching With Personal Training",
+    "excerpt":"this is just another test for collections  ","url": "https://jude-shin.github.io/_posts/2025-08-23-balancing-coaching-with-personal-training.html"
+  },{
+    "title": "Youth Nationals",
+    "excerpt":"this is just a test to see how collections work. For now, there is nothing to see :)  ","url": "https://jude-shin.github.io/_posts/2025-08-23-youth-nationals.html"
+  },{
+    "title": "About",
+    "excerpt":"Compuer Science I am currently pursuing my Bachelors in Computer Science at California Polytechnic State University. During the school year I work part time as a software engineer, and during the summer I work full time. Rock Climbing Through all of this, I also try and make time to continue...","url": "https://jude-shin.github.io/about/"
+  },{
+    "title": "bloggggg",
+    "excerpt":"This is just a test  lorum ipsum and all that greek stuff…   this is another test  lorum ipsum and all that greek stuff… (for the second time)   ","url": "https://jude-shin.github.io/blog/"
+  },{
+    "title": "Github Repo Organizer",
+    "excerpt":"This page primarily here so I can easily organize important github repos (github doesn’t have a way to do this). Maybe I will automate this somehow at somepoint. That would be cool. Personal jude-shin.github.io A personal website and online resume. .dotfiles Dotfiles for my personal machine. bin Scripts for my...","url": "https://jude-shin.github.io/githubrepos/"
+  },{
+    "title": "Welcome!",
+    "excerpt":"                                                                                                                     Rock Climbing                                                    See my journey as an outdoor rock climber, competitive climber, and youth competition coach.                                                   Read More                                                                                                                                                    Computer Science                                                    Take a look at some of the work I have done in this field.                                                   Read More                                                                                                                                                    Photography                                                    Just a gallery of my little side hobby.                                                   See Gallery                                        ","url": "https://jude-shin.github.io/"
+  }]
