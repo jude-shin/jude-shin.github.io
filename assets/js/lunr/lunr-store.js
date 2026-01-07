fbf6a48ada56ecad943dab681bfd70b462ed091b
jude-shin <jude-shin@users.noreply.github.com>
2026-01-07 21:27:06 +0000

    jekyll build from Action 3835197b94994c82210cc915f9e8a1f8081684ec

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,49 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["vim"],
+        "tags": [],
+        "url": "/vim/2025/08/10/welcome-to-jekyll.html",
+        "teaser": null
+      },{
+        "title": "Balancing Coaching With Personal Training",
+        "excerpt":"this is just another test for collections  ","categories": ["climbing","coaching"],
+        "tags": [],
+        "url": "/climbing/coaching/2025/08/10/balancing-coaching-with-personal-training.html",
+        "teaser": null
+      },{
+        "title": "Youth Nationals",
+        "excerpt":"this is just a test to see how collections work. For now, there is nothing to see :)  ","categories": ["climbing"],
+        "tags": [],
+        "url": "/climbing/2025/08/10/youth-nationals.html",
+        "teaser": null
+      },{
+    "title": "Welcome to Jekyll!",
+    "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","url": "https://jude-shin.github.io/_posts/2025-08-10-welcome-to-jekyll.html"
+  },{
+    "title": "Balancing Coaching With Personal Training",
+    "excerpt":"this is just another test for collections  ","url": "https://jude-shin.github.io/_posts/2025-08-23-balancing-coaching-with-personal-training.html"
+  },{
+    "title": "Youth Nationals",
+    "excerpt":"this is just a test to see how collections work. For now, there is nothing to see :)  ","url": "https://jude-shin.github.io/_posts/2025-08-23-youth-nationals.html"
+  },{
+    "title": "About",
+    "excerpt":"Compuer Science I am currently pursuing my Bachelors in Computer Science at California Polytechnic State University. During the school year I work part time as a software engineer, and during the summer I work full time. Rock Climbing Through all of this, I also try and make time to continue...","url": "https://jude-shin.github.io/about/"
+  },{
+    "title": "bloggggg",
+    "excerpt":"This is just a test  lorum ipsum and all that greek stuff…   this is another test  lorum ipsum and all that greek stuff… (for the second time)   ","url": "https://jude-shin.github.io/blog/"
+  },{
+    "title": "About",
+    "excerpt":"TODO  lorum ipsum or whatever every default page says.   Does it Only update  when there is enough content???   That would be pretty stupid  but then again, I should try writing a tool like this   ","url": "https://jude-shin.github.io/climbing/"
+  },{
+    "title": "About",
+    "excerpt":"TODO lorum ipsum or whatever every default page says. here is another subsection I wonder if this is really what is going on I want to know if I should use something else maybe I should just write my own raw html/css static page and host it on github pages...","url": "https://jude-shin.github.io/compsci/"
+  },{
+    "title": "Github Repo Organizer",
+    "excerpt":"This page primarily here so I can easily organize important github repos (github doesn’t have a way to do this). Maybe I will automate this somehow at somepoint. That would be cool. Personal jude-shin.github.io A personal website and online resume. .dotfiles Dotfiles for my personal machine. bin Scripts for my...","url": "https://jude-shin.github.io/githubrepos/"
+  },{
+    "title": "Welcome!",
+    "excerpt":"                                                                                                                     Rock Climbing                                                    See my journey as an outdoor rock climber, competitive climber, and youth competition coach.                                                   Read More                                                                                                                                                    Computer Science                                                    Take a look at some of the work I have done in this field.                                                   Read More                                                                                                                                                    Photography                                                    Just a gallery of my little side hobby.                                                   See Gallery                                        ","url": "https://jude-shin.github.io/"
+  },{
+    "title": "photography",
+    "excerpt":"Tools: I found an old Cannon AE-1 Program in my mom’s closet a while ago and decided to shoot some film on it. I had no idea what I was doing, and it was a miracle that I didn’t ruin the film that was sitting there for 10+ years. After...","url": "https://jude-shin.github.io/photography/"
+  }]
